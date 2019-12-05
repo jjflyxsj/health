44af5134acacdc35ff5b3d17c97c6e6f7bbe0bc4
pigxx <2534708135@qq.com>
2019-12-05 11:43:05 +0800

    删除

--- health_interface/src/main/java/com/itheima/service/MenuService.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface MenuService {
-    List<Map> findMenu();
-
-}
--- health_service_provider/src/main/java/com/itheima/dao/MenuDao.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.dao;
-
-import java.util.List;
-import java.util.Map;
-
-public interface MenuDao {
-    List<Map> findMenu();
-
-}
